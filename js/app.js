d4f05750205b1cb1427f8d5537d746b20c96a7f7
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-26 17:01:54 +0800

    loader spiner section

--- js/app.js
+++ js/app.js
@@ -150,7 +150,159 @@
 
 
 
-/*__________________________ step-5 ___________________________*/
+// /*__________________________ step-5 ___________________________*/
+// const loadPhones = async (searchValue) => {
+//     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchValue} `
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     // display 20 phones only for using slice(0,20)
+//     displayLoadPhones(data.data.slice(0, 20))  // always find array of object
+// }
+
+// const displayLoadPhones = datas => {
+//     // console.log(datas)
+//     // (step-1): get id from a parent container
+//     const phonesContainer = document.getElementById('phones-container');
+//     phonesContainer.innerHTML = '';
+//     /*___________________________________________________________________________*/
+//     // display no phone alrt message
+//     const noPhoneFound = document.getElementById('no-phone-found');
+//     if (datas.length === 0) {
+//         noPhoneFound.classList.remove('d-none')
+//     }
+//     else {
+//         noPhoneFound.classList.add('d-none')
+//     }
+//     /*___________________________________________________________________________*/
+//     // display all phones
+//     datas.forEach(singleData => {
+//         // console.log(singleData)
+//         console.log(singleData)
+
+//         // (step-2):create a phoneDiv, phoneDiv with classList.add('') and set innerHTML
+//         const phoneDiv = document.createElement('div');
+//         phoneDiv.classList.add('col');
+//         phoneDiv.innerHTML = `
+//             <div class="card h-100 w-100 p-3 ">
+//             <img src="${singleData.image}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">Name:${singleData.phone_name}</h5>
+//                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+//                     to additional content. This content is a little bit longer.</p>
+//             </div>
+//         </div>
+//             `;
+//         // (step-3):the parent container (step-1) with appendChild to the phoneDiv
+//         phonesContainer.appendChild(phoneDiv);
+//     });
+// };
+
+// // for btn search and  with search input field part
+// document.getElementById('btn-search').addEventListener('click', function () {
+//     const searchField = document.getElementById('search-field');
+//     const searchValue = searchField.value;
+//     searchField.value = '';
+
+//     // set loadPhones call function here and dynamic loadPhones url
+//     loadPhones(searchValue)
+
+// });
+
+//     // call function
+//     // loadPhones();   // for no phone found message alrt you have to close
+
+
+
+
+// /*__________________________ step-5 ___________________________*/
+// const loadPhones = async (searchValue) => {
+//     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchValue} `
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     // display 20 phones only for using slice(0,20)
+//     displayLoadPhones(data.data.slice(0, 20))  // always find array of object
+// }
+
+// const displayLoadPhones = datas => {
+//     // console.log(datas)
+//     // (step-1): get id from a parent container
+//     const phonesContainer = document.getElementById('phones-container');
+//     phonesContainer.innerHTML = '';
+//     /*___________________________________________________________________________*/
+//     // display no phone alrt message
+//     const noPhoneFound = document.getElementById('no-phone-found');
+//     if (datas.length === 0) {
+//         noPhoneFound.classList.remove('d-none')
+//     }
+//     else {
+//         noPhoneFound.classList.add('d-none')
+//     }
+//     /*___________________________________________________________________________*/
+//     // display all phones
+//     datas.forEach(singleData => {
+//         // console.log(singleData)
+//         console.log(singleData)
+
+//         // (step-2):create a phoneDiv, phoneDiv with classList.add('') and set innerHTML
+//         const phoneDiv = document.createElement('div');
+//         phoneDiv.classList.add('col');
+//         phoneDiv.innerHTML = `
+//             <div class="card h-100 w-100 p-3 ">
+//             <img src="${singleData.image}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">Name:${singleData.phone_name}</h5>
+//                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+//                     to additional content. This content is a little bit longer.</p>
+//             </div>
+//         </div>
+//             `;
+//         // (step-3):the parent container (step-1) with appendChild to the phoneDiv
+//         phonesContainer.appendChild(phoneDiv);
+//     });
+//      /*-------------- stop loader  ---------------------*/ 
+//     // stop loader: call function toggleLoader
+//     toggleLoader(false);
+//     /*----------------------------------------------------------*/
+// };
+
+
+
+// // for btn search and  with search input field part
+// document.getElementById('btn-search').addEventListener('click', function () {
+//     /*-------------- start loader  ---------------------*/ 
+//     // start loader: call function toggleLoader
+//     toggleLoader(true);
+//     /*----------------------------------------------------------*/ 
+//     const searchField = document.getElementById('search-field');
+//     const searchValue = searchField.value;
+//     searchField.value = '';
+
+//     // set loadPhones call function here and dynamic loadPhones url
+//     loadPhones(searchValue)
+
+// });
+
+//  /*_________________________ toggle-Loader part _________________________________*/
+//  const toggleLoader = isLodding => {
+// const toggleLoaderSection= document.getElementById('toggle-Loader');
+// if(isLodding){
+//     toggleLoaderSection.classList.remove('d-none')
+// }
+// else{
+//     toggleLoaderSection.classList.add('d-none')
+// }
+
+//  }
+//   /*_______________________________________________________________________*/
+//     // call function
+//     // loadPhones();   // for no phone found message alrt you have to close
+
+
+
+
+
+
+/*__________________________ step-6 ___________________________*/
 const loadPhones = async (searchValue) => {
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchValue} `
     const res = await fetch(url);
@@ -195,10 +347,20 @@ const displayLoadPhones = datas => {
         // (step-3):the parent container (step-1) with appendChild to the phoneDiv
         phonesContainer.appendChild(phoneDiv);
     });
+     /*-------------- stop loader  ---------------------*/ 
+    // stop loader: call function toggleLoader
+    toggleLoader(false);
+    /*----------------------------------------------------------*/
 };
+
+
 
 // for btn search and  with search input field part
 document.getElementById('btn-search').addEventListener('click', function () {
+    /*-------------- start loader  ---------------------*/ 
+    // start loader: call function toggleLoader
+    toggleLoader(true);
+    /*----------------------------------------------------------*/ 
     const searchField = document.getElementById('search-field');
     const searchValue = searchField.value;
     searchField.value = '';
@@ -208,5 +370,17 @@ document.getElementById('btn-search').addEventListener('click', function () {
 
 });
 
+ /*_________________________ toggle-Loader part _________________________________*/
+ const toggleLoader = isLodding => {
+const toggleLoaderSection= document.getElementById('toggle-Loader');
+if(isLodding){
+    toggleLoaderSection.classList.remove('d-none')
+}
+else{
+    toggleLoaderSection.classList.add('d-none')
+}
+
+ }
+  /*_______________________________________________________________________*/
     // call function
     // loadPhones();   // for no phone found message alrt you have to close
